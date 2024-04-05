4f4628f27a1da3cef7d55958ba3110ed0aee2e9c
Masibonge Masinga <masi.masinga@resolute.education>
2024-04-05 13:53:45 +0200

    wip - crud for order apis

--- src/base/serializers.py
+++ src/base/serializers.py
@@ -1,6 +1,54 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import User
 from .models import Product, Review, Order, OrderItem, ShippingAddress
+    
+
+class UserSerializer(serializers.ModelSerializer):
+    _id = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+    
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializers = UserSerializerWithToken(self.user).data
+        
+        for k, v in serializers.items():
+            data[k] = v
+
+        return data
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
